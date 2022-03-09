a746572918aea46630092607a9a7d588dd6fecf2
Kevin Ruiz <kevin.rf.es@gmail.com>
2022-03-09 15:16:37 +0000

    Añadido el modulo logica

---
+++ logica/src/main/java/es/iespuerto/ets/App.java
@@ -0,0 +1,13 @@
+package es.iespuerto.ets;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
