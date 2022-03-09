190a7447a2602d8175831e7d33864cb1877f52c6
Kevin Ruiz <kevin.rf.es@gmail.com>
2022-03-09 15:19:13 +0000

    Añadido el modulo integracion

---
+++ integracion/src/test/java/es/iespuerto/ets/AppTest.java
@@ -0,0 +1,20 @@
+package es.iespuerto.ets;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
